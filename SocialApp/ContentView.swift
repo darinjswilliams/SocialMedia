29a8c27bb24cfc14bafa8e6ef02d8c0b43b8582c
darinjswilliams <darinjswilliams@icloud.com>
2024-09-07 23:41:00 -0500

    adding refactor files

--- SocialApp/ContentView.swift
+++ SocialApp/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isFavorite: Bool = true
-    @State private var heartColor = Color.black
+   
     let title = "Sunset Beach..Aww"
     let description = "The Beach at Ft Lauderdale is the best we had a lovely evening. Nice Company, Great Food"
     
     var body: some View {
         VStack{
             
-            Text(title)
+           PostDescriptView(title: title, description: description)
             List {
                 // Add your social media post here
                 UserPhotoView()
@@ -24,31 +24,62 @@ struct ContentView: View {
                 UserPhotoView()
             }
             .listStyle(.plain)
-            
-            //            You don't need to set .foregroundColor if you use it as Button label. It will automatically pick up
-            //            from the parent's accentColor. This is a normal behavior of a button in SwiftUI.
-            VStack(alignment: .leading, spacing: 15) {
-
-                HStack{
-                    
-                    Button(action: {
-                        
-                        isFavorite.toggle()
-                        
-                    }, label: {
-                        Image(systemName: "heart.fill")
-                    })
-                    .accentColor(Color(isFavorite ? heartColor: Color.red ))
-                    
-                    
-                }
-                Text(description)
-                
-            }
+            FavoriteButtonView(isFavorite: $isFavorite)
             
         }
     }
 
+}
+
+
+struct PostDescriptView: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        HStack{
+            VStack{
+                Text(title)
+                    .bold()
+                    .font(.title)
+                Text(description)
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            .multilineTextAlignment(.leading)
+            Spacer()
+        }
+    }
+}
+
+struct FavoriteButtonView: View {
+    
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        VStack {
+
+            HStack{
+                
+                // You don't need to set .foregroundColor if you use it as Button label. It will automatically pick up
+                // from the parent's accentColor. This is a normal behavior of a button in SwiftUI.
+                // This can be updated
+                
+                Button(action: {
+                    
+                    isFavorite.toggle()
+                    
+                }, label: {
+                    Image(systemName: "heart")
+                        .symbolVariant(isFavorite ? .fill : .none)
+                        .imageScale(.large)
+                        .foregroundColor(.red)
+                })
+                .buttonStyle(.plain)
+            }
+           
+        }
+    }
 }
 
 
